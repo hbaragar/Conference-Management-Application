a74af4bac3082f739ffc185e4c7bff3b11545389
Henry Baragar <henry@henry.(none)>
2010-01-29 16:01:25 -0500

    Added JosMenu model.

---
+++ app/models/jos_menu.rb
@@ -0,0 +1,19 @@
+class JosMenu < ActiveRecord::Base
+
+  set_table_name 'jos_menu'
+  #alias_attribute :parent, :parent_id
+
+  def before_validation
+    self.checked_out_time = Time.now
+    self.alias = name.tr("A-Z","a-z").gsub(/\W+/,"-") unless self.alias[/\w/] if name
+  end
+
+  #acts_as_tree :column => 'parent'
+  acts_as_list :column => :ordering
+
+  validates_presence_of :name
+  validates_uniqueness_of :name, :scope => :parent
+  validates_format_of :alias, :with => /^[-\w]+/
+  validates_uniqueness_of :alias, :scope => :parent
+
+end
