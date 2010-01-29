1aabe8908d46e027ad5b6e31321c8ce48614588e
Henry Baragar <henry@henry.(none)>
2010-01-29 16:23:44 -0500

    Removed dead comments.

--- app/models/joomla_menu.rb
+++ app/models/joomla_menu.rb
@@ -1,14 +1,12 @@
 class JoomlaMenu < ActiveRecord::Base
 
   set_table_name 'jos_menu'
-  #alias_attribute :parent, :parent_id
 
   def before_validation
     self.checked_out_time = Time.now
     self.alias = name.tr("A-Z","a-z").gsub(/\W+/,"-") unless self.alias[/\w/] if name
   end
 
-  #acts_as_tree :column => 'parent'
   acts_as_list :column => :ordering
 
   validates_presence_of :name
