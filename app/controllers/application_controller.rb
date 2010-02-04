989df3f3d060fd31d9f24c7def1df823a4f6b88b
Splash UAT & Production User <splash@instantiated.ca>
2010-02-04 14:36:15 -0500

    Now redirecting users that are going into forbidden territory.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,17 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
+  def rescue_action_in_public exception
+    case exception
+    when Hobo::PermissionDeniedError 
+      redirect_to :controller => :front, :action => :index
+    else
+      # **TODO Fix to the "when" to catch the correct exception;
+      # in the meantime, catch every exception
+      redirect_to :controller => :front, :action => :index
+    end
+  end
+
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 end
