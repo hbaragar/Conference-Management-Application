eb2e67ad73f763d873f10c9188121e5582fbba04
Henry Baragar <henry@henry-Latitude-E6430.(none)>
2012-08-14 12:08:01 -0400

    Change afternoon session back to starting at 1:30pm.

--- app/models/day.rb
+++ app/models/day.rb
@@ -205,8 +205,8 @@ class Day
     start_times = [
       date + 8.hours + 30.minutes,
       date + 10.hours + 30.minutes,
-      date + 14.hours + 00.minutes,
-      date + 16.hours + 00.minutes
+      date + 14.hours + 30.minutes,
+      date + 15.hours + 30.minutes
     ]
     start_times.collect do |st|
       et = st + duration.minutes
